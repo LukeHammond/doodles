4077930f203732ca24c45feeab0fc236e22caab5
LukeHammond <luke.e.j.hammond@gmail.com>
2012-04-06 09:56:38 -0400

    initial commit of basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doodles::Application.config.secret_token = '76e534565748964f1169cd33e3b1a16095351fe609e69de2b8d4e57be14708f5ec027b67e69a939a9d415c329833b18d5cd3d030e41b547b441b518152006647'
