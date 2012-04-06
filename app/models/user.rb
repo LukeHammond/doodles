f41060d2f70503674574bbe1408bd4170e24d8a6
LukeHammond <luke.e.j.hammond@gmail.com>
2012-04-06 10:29:27 -0400

    refactor validation and reset_token lookup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,28 @@
 class User < ActiveRecord::Base
+  extend ActiveSupport::Memoizable
+
   has_many :reset_tokens
   
   def reset_password(params)
-    raise 'invalid token' if !params[:token].present? || !(token = ResetToken.find_by_slug(params[:token])) || (id != token.user_id)
+    raise 'invalid token' unless passes_validation?(params)
 
     pass = params[:pass]
     pass_confirmation = params[:pass_confirmation]
     save
     unlock_login
-    token.update_attribute(:used_at, Time.now)
+    reset_token(params[:token]).update_attribute(:used_at, Time.now)
   end
   
   def unlock_login
     true
   end
+
+  def passes_validation?(params)
+    params[:token].present? && reset_token(params[:token]).present?
+  end
+
+  def reset_token(token)
+    self.reset_tokens.find_by_slug(token)
+  end
+  memoize :reset_token
 end
